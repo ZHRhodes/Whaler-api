5509e169c4c80576eacaed30beafe6f08f3f17fe
Zachary Rhodes <zhrhodes@gmail.com>
2020-04-13 00:24:35 -0700

    moved print

--- app/auth.go
+++ app/auth.go
@@ -42,8 +42,6 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 
 		splitted := strings.Split(tokenHeader, ".")
 		if len(splitted) != 3 {
-			fmt.Print(fmt.Sprint(tokenHeader))
-			fmt.Print(fmt.Sprint(len(splitted)))
 			response = utils.Message(4003, "Invalid/Malformed auth token", true, map[string]interface{}{})
 			w.WriteHeader(http.StatusForbidden)
 			w.Header().Add("Content-Type", "application/json")
@@ -59,6 +57,7 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 		})
 
 		if err != nil {
+			fmt.Print(fmt.Sprint(err))
 			response = utils.Message(4003, "Malformed authentication token", true, map[string]interface{}{})
 			w.WriteHeader(http.StatusForbidden)
 			w.Header().Add("Content-Type", "application/json")
