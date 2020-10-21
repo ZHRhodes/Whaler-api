750ccd41256884a2aa7d77c102a4af1a174cc4be
Zachary Rhodes <zhrhodes@gmail.com>
2020-10-21 01:41:20 -0700

    fix case of column names

--- models/account.go
+++ models/account.go
@@ -60,8 +60,8 @@ func SaveAccounts(newAccounts []*model.NewAccount) ([]*Account, error) {
 	err := DB().Clauses(clause.OnConflict{
 		Columns: []clause.Column{{Name: "id"}},
 		DoUpdates: clause.AssignmentColumns([]string{"name", "owner", "industry",
-			"salesforce_id", "industry", "description", "numberOfEmployees", "annualRevenue",
-			"billingCycle", "billingState", "phone", "website", "type", "state", "notes"}),
+			"salesforce_id", "industry", "description", "number_of_employees", "annual_revenue",
+			"billing_cycle", "billing_state", "phone", "website", "type", "state", "notes"}),
 	}).Create(&accounts).Error
 
 	// err := DB().Clauses(clause.OnConflict{DoNothing: true}).Create(&accounts).Error
